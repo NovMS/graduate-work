b22eb889988cca724c73d1aa76c8a0f91ab12ced
Maksim Novoseltsev <nov.ms@bk.ru>
2020-03-11 22:24:39 +0300

    init

---
+++ web/src/client/components/header.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { Typography } from '@material-ui/core';
+import styled from 'styled-components';
+
+const Root = styled.header`
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  height: 50px;
+  width: 100%;
+  background-color: #1976d2;
+  box-shadow: 0 1px 2px rgba(0, 0, 0, 0.24);
+  color: #fff;
+  position: relative;
+`;
+
+const Name = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  position: absolute;
+  left: 30px;
+`;
+
+const ShortName = styled(Typography)`
+  font-size: 30px;
+  font-weight: bold;
+`;
+
+const FullName = styled(Typography)`
+  margin-top: 2px;
+  font-size: 12px;
+  line-height: 13px;
+  margin-left: 3px;
+`;
+
+const Header = ({ page }) => {
+  return (
+    <Root>
+      <Name>
+        <ShortName>АРСВ</ShortName>
+        <FullName>Аэродинамический расчет<br/>систем вентиляции</FullName>
+      </Name>
+    </Root>
+  );
+};
+
+export default Header;
---
+++ web/src/client/components/pages/main.js
@@ -0,0 +1,47 @@
+import React, { useState, useEffect } from 'react';
+import { getQuery } from '../../services/query-service'
+import { makeStyles } from '@material-ui/core/styles';
+import styled from 'styled-components';
+
+import Header from '../header';
+import Chart from '../chart'
+import Menu from '../menu'
+
+import CircularProgress from '@material-ui/core/CircularProgress';
+
+const Root = styled.div`
+  width: 100%;
+  background-color: rgb(245, 245, 245);
+`;
+
+const Content = styled.div`
+  width: 100%;
+  display: flex;
+  height: calc(100vh - 50px);
+  justify-content: center;
+`;
+
+const Main = () => {
+	const [data, setData] = useState();
+
+	useEffect(() => {
+		(async() => {
+			const response = await getQuery(`/getDataForChart`, { name: 'users_and_sessions', context: 'metrics', networking: 'all', set: 'all', source: 'all'});
+			setData(response);
+		})();
+	},[])
+
+	return (
+	  <Root>
+	    <Header page="main"/>
+	    <Content>
+			<Menu />
+			{
+				data ? <Chart title="Количество пользователей и сессий" data={data} /> : <CircularProgress />
+			}
+	    </Content>
+	  </Root>
+  )
+}
+
+export default Main;
